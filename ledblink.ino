19bf53636ca0e2ef3a25ba8cd90a7cf940a90de2
MarcusForG <mg7631@nyu.edu>
2025-11-04 13:21:07 -0500

    leds blink

--- ledblink.ino
+++ ledblink.ino
@@ -2,10 +2,15 @@ int ledPin = 33;
 int ledPin2 = 34;
 
 void setup() {
-pinMode(ledPin, OUTPUT);
-pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
-
+  digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin2, HIGH)
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin2, LOW)
+  delay(500);
 }
